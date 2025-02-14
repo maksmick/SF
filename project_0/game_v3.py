ae720e0ee796cd109c8163f039ef31089ba070f2
MickMak <maksenoff@yahoo.com>
2025-02-14 13:10:25 +0300

    Add random number guessing algorithm and scoring function

---
+++ project_0/game_v3.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+
+    count = 0
+    low, high = 1, 100  # диапазон чисел от 1 до 100
+
+    while True:
+        count += 1
+        predict = (low + high) // 2  # находим середину диапазона
+
+        if predict == number:
+            return count  # число угадано
+        elif predict < number:
+            low = predict + 1  # сужаем диапазон снизу
+        else:
+            high = predict - 1  # сужаем диапазон сверху
+
+
+print(f'Количество попыток: {random_predict()}')
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+
+    count_ls = []  # список для сохранения количества попыток
+    np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(
+        1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))  # находим среднее количество попыток
+
+    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    return (score)
+
+
+# RUN
+score_game(random_predict)
+
+if __name__ == '__main__':
+    score_game(random_predict)
